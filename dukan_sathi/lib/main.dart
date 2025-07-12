ef6f1ffda73d44b8aa65fdafb26e57d28b76739a
Durgesh Kanzariya <durgesh.j.kanzariya@gmail.com>
2025-07-12 22:44:08 +0530

    created onboard page

--- dukan_sathi/lib/main.dart
+++ dukan_sathi/lib/main.dart
@@ -9,10 +9,45 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color.fromARGB(255, 250, 244, 232),
+      ),
       home: Scaffold(
         body: Center(
-          child: Text('Hello Worggregbkgbekgs'),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset("assets/images/logo.png"),
+              Text(
+                """Your grocery ready
+with us.""",
+                style: TextStyle(
+                  color: Color.fromARGB(255, 57, 66, 61),
+                  fontSize: 24,
+                ),
+              ),
+              SizedBox(height: 35),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green, // ✅ Button background color
+                  foregroundColor: Colors.white, // ✅ Text/Icon color
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Get Started"),
+                    SizedBox(width: 10),
+                    Image.asset("assets/images/Arrow 4.png"),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
