be660a93e16f216599de0df2951fe23cc75f0231
Durgesh Kanzariya <durgesh.j.kanzariya@gmail.com>
2025-07-13 00:03:12 +0530

    Completed onboarding page

---
+++ dukan_sathi/lib/login_page.dart
@@ -0,0 +1,23 @@
+// import 'package:flutter/material.dart';
+
+// class LoginPage extends StatelessWidget {
+//   const LoginPage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Login")),
+      body: const Center(child: Text("Login Page")),
+    );
+  }
+}
--- dukan_sathi/lib/main.dart
+++ dukan_sathi/lib/main.dart
@@ -1,3 +1,4 @@
+import 'onboarding_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,60 +15,7 @@ class MainApp extends StatelessWidget {
       theme: ThemeData(
         scaffoldBackgroundColor: const Color.fromARGB(255, 250, 244, 232),
       ),
-      home: Scaffold(
-        body: Stack(
-          children: [
-            Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset("assets/images/logo.png"),
-                  const Text(
-                    """Your grocery ready 
-with us.""",
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 57, 66, 61),
-                      fontSize: 24,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 50.0),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                    foregroundColor: Colors.white,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 100,
-                      vertical: 15,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    elevation: 5,
-                  ),
-                  child: const Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text("Get Started", style: TextStyle(fontSize: 16)),
-                      SizedBox(width: 10),
-                      Image(
-                        image: AssetImage("assets/images/Arrow 4.png"),
-                        height: 24,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      home: OnboardingPage(),
     );
   }
 }
