d7df20c9bb38a71b61cd2a042e3e5ac88f7dcc83
KISHAN SADHU <kishansadhusadhu69@gmail.com>
2025-07-12 13:47:55 +0530

    test_for_getstaeted_page

---
+++ dukan_sathi/lib/test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+          body: Container(
+          color: const Color.fromARGB(255, 237, 235, 166),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              spacing: 20,
+              children: [
+                Container(
+                  height: 300,
+                  width: 300,
+                  child: Image.asset("assets/imgs/one.jpg"),
+                ),
+                Text("Your Grocery Ready With Us"),
+
+                ElevatedButton(
+                  child: Text("Get Started  ->"),
+                  onPressed: () {
+                    print("");
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    foregroundColor: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
