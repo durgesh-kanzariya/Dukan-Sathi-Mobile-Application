af564e63242278a53b0854b275be15ac1ee770e0
Durgesh Kanzariya <durgesh.j.kanzariya@gmail.com>
2025-07-13 00:54:48 +0530

    started login page

--- dukan_sathi/lib/login_page.dart
+++ dukan_sathi/lib/login_page.dart
@@ -16,8 +16,44 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Login")),
-      body: const Center(child: Text("Login Page")),
+      body: Stack(
+        children: [
+          // Vector 1 at the top
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Image.asset("assets/images/Vector 1.png", fit: BoxFit.fill),
+          ),
+
+          // Vector 2 at the bottom-left
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: Image.asset("assets/images/Vector 2.png"),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Login',
+                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 20),
+                  TextField(),
+                  const SizedBox(height: 12),
+                  TextField(obscureText: true),
+                  const SizedBox(height: 20),
+                  ElevatedButton(onPressed: () {}, child: Text('Login')),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
