f4de54b0c034d1ea8696e20709eb93e7023035e2
KISHAN SADHU <kishansadhusadhu69@gmail.com>
2025-07-14 20:07:05 +0530

    Login_Page_Test

--- dukan_sathi/lib/main.dart
+++ dukan_sathi/lib/main.dart
@@ -9,11 +9,77 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello Worggregbkgbekgs'),
-        ),
+        body: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+              Image.asset("assets/imgs/1.png", height: 150),
+
+              Text("Hello", style: TextStyle(fontSize: 70)),
+              Text(
+                "Sign In To Your Account",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
+              ),
+
+              Text(""),
+
+              Text("Select Owner"),
+              Padding(padding:EdgeInsetsGeometry.fromLTRB(30, 0, 30, 0), child: Card(shadowColor: Colors.black, elevation: 5, child: TextField())),
+
+              Text("Username"),
+              Padding(padding:EdgeInsetsGeometry.fromLTRB(30, 0, 30, 0), child: Card(shadowColor: Colors.black, elevation: 5, child: TextField())),
+
+              Text("Password "),
+              Padding(padding:EdgeInsetsGeometry.fromLTRB(30, 0, 30, 0), child: Card(shadowColor: Colors.black, elevation: 5, child: TextField())),
+
+              Container(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: EdgeInsetsGeometry.all(10),
+                  child: Text("Forget Password?"),
+                ),
+              ),
+
+              Container(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: EdgeInsetsGeometry.all(10),
+                  child: Row(
+                    spacing: 10,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text("Sign In",style: TextStyle(fontSize: 20),),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.green,
+                        ),
+                        onPressed: () {
+                          print("");
+                        },
+                        child: Text("-->"),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+            Container(
+              alignment: Alignment.center,
+              child: Text("Don’t have an account? Create"),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image.asset("assets/imgs/2.jpg", height: 160),
+              ],
+            ),
+
+            ],
+          ),
       ),
     );
   }
